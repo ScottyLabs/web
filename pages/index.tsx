6d501e7d7719edfd092d76558ca4f088b6b135ef
Gram Liu <gram.liu226@gmail.com>
2022-07-18 23:10:57 -0700

    Create hero icon with gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Gradient from "../src/components/Gradient";
 import Navbar from "../src/components/Navbar";
+import styles from "../styles/Home.module.scss";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -11,8 +13,18 @@ const Home: NextPage = () => {
         <meta name="description" content="ScottyLabs" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Navbar />
       <Gradient />
+      <Navbar />
+      <main className={styles.container}>
+        <div className={styles.heroImage}>
+          <Image
+            width={350}
+            height={350}
+            src="/icons/scotty-dog.svg"
+            alt="ScottyLabs icon"
+          />
+        </div>
+      </main>
     </div>
   );
 };
