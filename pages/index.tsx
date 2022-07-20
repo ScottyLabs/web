5247e2d8bf2c7ace9d7da7f696ec1c1938614630
Gram Liu <gram.liu226@gmail.com>
2022-07-19 21:20:16 -0700

    Change navbar into hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Gradient from "../src/components/Gradient";
 import Navbar from "../src/components/Navbar";
-import styles from "../styles/Home.module.scss";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -13,28 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="ScottyLabs" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className={styles.gradientContainer}>
-        <Gradient className={styles.gradient} />
-        <svg width={0} height={0}>
-          <defs>
-            <clipPath id="gradientClipPath" clipPathUnits="objectBoundingBox">
-              <path d="M0,0.8 C0.4,1 0.6,0.5 1,0.7 L1,0 L0,0 Z" />
-            </clipPath>
-          </defs>
-        </svg>
-      </div>
-      <Navbar />
-      <main className={styles.mainContainer}>
-        <div className={styles.heroImage}>
-          <Image
-            width={350}
-            height={350}
-            src="/icons/scotty-dog.svg"
-            alt="ScottyLabs icon"
-          />
-        </div>
-        <div className={styles.heroTitle}>ScottyLabs</div>
-      </main>
+      <Navbar home />
     </div>
   );
 };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,28 @@
-import Link from "next/link";
-import React, { ReactElement } from "react";
-import styles from "./index.module.scss";
 import Image from "next/image";
+import Link from "next/link";
+import Gradient from "../Gradient";
+import styles from "./index.module.scss";
 
-const Navbar = () => {
+/**
+ * Toolbar containing just the navigation elements without
+ * the gradient background
+ */
+const Toolbar = ({ showHomeIcon = true }: { showHomeIcon?: Boolean }) => {
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.navbarLeft}>
-        <Link href="/" passHref>
-          <a>
-            <Image
-              width={32}
-              height={32}
-              src="/icons/scotty-dog.svg"
-              alt="ScottyLabs icon"
-            />
-          </a>
-        </Link>
+        {showHomeIcon ? (
+          <Link href="/" passHref>
+            <a>
+              <Image
+                width={32}
+                height={32}
+                src="/icons/scotty-dog.svg"
+                alt="ScottyLabs icon"
+              />
+            </a>
+          </Link>
+        ) : null}
       </div>
       <div className={styles.navbarRight}>
         <Link href="/about" passHref>
@@ -31,6 +37,39 @@ const Navbar = () => {
         <Link href="/events" passHref>
           <a className={`link ${styles.navLink}`}>/events</a>
         </Link>
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Final navbar with a gradient background.
+ * If `home` is true, this expands to an entire hero component
+ */
+const Navbar = ({ home = false }: { home?: Boolean }) => {
+  return (
+    <div>
+      <div className={styles.gradientContainer}>
+        <Gradient className={styles.gradient} />
+        <svg width={0} height={0}>
+          <defs>
+            <clipPath id="gradientClipPath" clipPathUnits="objectBoundingBox">
+              <path d="M0,0.8 C0.4,1 0.6,0.5 1,0.7 L1,0 L0,0 Z" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      <Toolbar showHomeIcon={!home} />
+      <div className={styles.heroContainer}>
+        <div className={styles.heroImage}>
+          <Image
+            width={350}
+            height={350}
+            src="/icons/scotty-dog.svg"
+            alt="ScottyLabs icon"
+          />
+        </div>
+        <div className={styles.heroTitle}>ScottyLabs</div>
       </div>
     </div>
   );
