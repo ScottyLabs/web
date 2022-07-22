b918a4afd03eabf43cab9c2c6a20ca5a6458afc0
Gram Liu <gram.liu226@gmail.com>
2022-07-21 23:43:34 -0700

    Change amp and speed

--- src/components/Gradient/index.tsx
+++ src/components/Gradient/index.tsx
@@ -24,6 +24,7 @@ const Gradient = ({
       gradient.freqX = 14e-5;
       gradient.freqY = 29e-5;
       gradient.freqDelta = 1e-4;
+      gradient.amp = 100;
       gradient.initGradient("#gradient-canvas");
     }
   }, [gradientColors]);
