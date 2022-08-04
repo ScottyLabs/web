337b55cb5c3fd5fb03ff47c0369cfd1b6eb05108
Gram Liu <gram.liu226@gmail.com>
2022-08-03 23:21:22 -0700

    Change Home to use default export

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,11 @@ import EventCarousel from "../src/components/EventCarousel";
 import Hero from "../src/components/Hero";
 import BaseLayout from "../src/layouts/BaseLayout";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <BaseLayout collapsedNavBar={false}>
       <Hero />
       <EventCarousel />
     </BaseLayout>
   );
-};
-
-export default Home;
+}
