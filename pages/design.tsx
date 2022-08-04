fca15b3405c029d4b794cb87a8344064d6440cdf
Gram Liu <gram.liu226@gmail.com>
2022-08-03 20:17:06 -0700

    Create design landing page

---
+++ pages/design.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from "next";
+import Image from "next/future/image";
+import Head from "next/head";
+import Footer from "../src/components/Footer";
+import Navbar from "../src/components/Navbar";
+import Typography from "../src/components/Typography";
+import AfterEffectsIcon from "../src/icons/AfterEffectsIcon";
+import FigmaIcon from "../src/icons/FigmaIcon";
+import IllustratorIcon from "../src/icons/IllustratorIcon";
+import ProcreateIcon from "../src/icons/ProcreateIcon";
+import styles from "../styles/Design.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>ScottyLabs</title>
+        <meta name="description" content="ScottyLabs" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className={styles.pageContainer}>
+        <main>
+          <Navbar collapsed={true} />
+          <div className={styles.pageContent}>
+            <Typography variant="header">Design</Typography>
+            <div className={styles.aboutSection}>
+              <Typography variant="body">
+                Design is a ScottyLabs team branch that works on the event
+                branding, merchandising, and UI designs for all ScottyLabs
+                projects and events.
+              </Typography>
+
+              <div className={styles.softwareIconContainer}>
+                <FigmaIcon className={styles.softwareIcon} />
+                <IllustratorIcon className={styles.softwareIcon} />
+                <ProcreateIcon className={styles.softwareIcon} />
+                <AfterEffectsIcon className={styles.softwareIcon} />
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Home;
