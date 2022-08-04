806a3898d5980841b36d019f90d9f7c7f5ff4dc4
Gram Liu <gram.liu226@gmail.com>
2022-08-03 23:25:57 -0700

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from "next";
 import EventCarousel from "../src/components/EventCarousel";
 import Hero from "../src/components/Hero";
 import BaseLayout from "../src/layouts/BaseLayout";
