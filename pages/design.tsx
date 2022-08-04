6b8482b2c4958a15e8d3bde9ca9ee128ead8c1fd
Gram Liu <gram.liu226@gmail.com>
2022-08-03 23:32:00 -0700

    Change Design to use BaseLayout

--- pages/design.tsx
+++ pages/design.tsx
@@ -1,47 +1,31 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Footer from "../src/components/Footer";
-import Navbar from "../src/components/Navbar";
 import Typography from "../src/components/Typography";
 import AfterEffectsIcon from "../src/icons/AfterEffectsIcon";
 import FigmaIcon from "../src/icons/FigmaIcon";
 import IllustratorIcon from "../src/icons/IllustratorIcon";
 import ProcreateIcon from "../src/icons/ProcreateIcon";
+import BaseLayout from "../src/layouts/BaseLayout";
 import styles from "../styles/Design.module.scss";
 
-const Design: NextPage = () => {
+export default function Design() {
   return (
-    <>
-      <Head>
-        <title>ScottyLabs</title>
-        <meta name="description" content="ScottyLabs" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <div className={styles.pageContainer}>
-        <main>
-          <Navbar collapsed={true} />
-          <div className={styles.pageContent}>
-            <Typography variant="header">Design</Typography>
-            <div className={styles.aboutSection}>
-              <Typography variant="body">
-                Design is a ScottyLabs team branch that works on the event
-                branding, merchandising, and UI designs for all ScottyLabs
-                projects and events.
-              </Typography>
+    <BaseLayout collapsedNavBar={true}>
+      <div className={styles.pageContent}>
+        <Typography variant="header">Design</Typography>
+        <div className={styles.aboutSection}>
+          <Typography variant="body">
+            Design is a ScottyLabs team branch that works on the event branding,
+            merchandising, and UI designs for all ScottyLabs projects and
+            events.
+          </Typography>
 
-              <div className={styles.softwareIconContainer}>
-                <FigmaIcon className={styles.softwareIcon} />
-                <IllustratorIcon className={styles.softwareIcon} />
-                <ProcreateIcon className={styles.softwareIcon} />
-                <AfterEffectsIcon className={styles.softwareIcon} />
-              </div>
-            </div>
+          <div className={styles.softwareIconContainer}>
+            <FigmaIcon className={styles.softwareIcon} />
+            <IllustratorIcon className={styles.softwareIcon} />
+            <ProcreateIcon className={styles.softwareIcon} />
+            <AfterEffectsIcon className={styles.softwareIcon} />
           </div>
-        </main>
-        <Footer />
+        </div>
       </div>
-    </>
+    </BaseLayout>
   );
-};
-
-export default Design;
+}
