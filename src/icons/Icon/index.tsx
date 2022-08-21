d7dce5029df021e6c95df514a142eb918b20b3de
Gram Liu <gram.liu226@gmail.com>
2022-08-21 00:59:10 -0400

    Revert "Move wrapProps to HOC inside Icon component"
    This reverts commit 16353d2cb4f09d62dd6d367cdba7e13077e8cfb1.

--- src/icons/Icon/index.tsx
+++ src/icons/Icon/index.tsx
@@ -1,4 +1,4 @@
-import IconWrapper from "../../utils/wrapProps";
+import wrapProps from "../../utils/wrapProps";
 import AfterEffectsIcon from "../AfterEffectsIcon";
 import FacebookIcon from "../FacebookIcon";
 import FigmaIcon from "../FigmaIcon";
@@ -13,41 +13,27 @@ import ScottyLabsIcon from "../ScottyLabsIcon";
 import TypeScriptIcon from "../TypeScriptIcon";
 
 const IconMapping = {
-  "after-effects": AfterEffectsIcon,
-  facebook: FacebookIcon,
-  figma: FigmaIcon,
-  illustrator: IllustratorIcon,
-  instagram: InstagramIcon,
-  mail: MailIcon,
-  mattermost: MattermostIcon,
-  medium: MediumIcon,
-  procreate: ProcreateIcon,
-  react: ReactIcon,
-  scottylabs: ScottyLabsIcon,
-  typescript: TypeScriptIcon,
+  "after-effects": <AfterEffectsIcon />,
+  facebook: <FacebookIcon />,
+  figma: <FigmaIcon />,
+  illustrator: <IllustratorIcon />,
+  instagram: <InstagramIcon />,
+  mail: <MailIcon />,
+  mattermost: <MattermostIcon />,
+  medium: <MediumIcon />,
+  procreate: <ProcreateIcon />,
+  react: <ReactIcon />,
+  scottylabs: <ScottyLabsIcon />,
+  typescript: <TypeScriptIcon />,
 };
 
 export type IconName = keyof typeof IconMapping;
-interface IconProps {
+
+interface Props {
   className?: string;
   name: IconName;
 }
 
-interface IconWrapperProps {
-  element: (props: any) => JSX.Element;
-  className?: string;
-}
-
-/**
- * Wrap an element to inject props
- */
-function IconWrapper({
-  element: Element,
-  className,
-}: IconWrapperProps): JSX.Element {
-  return <Element className={className} />;
-}
-
-export default function Icon({ name, className }: IconProps) {
-  return <IconWrapper element={IconMapping[name]} className={className} />;
+export default function Icon({ className, name }: Props) {
+  return wrapProps(IconMapping[name])({ className });
 }
