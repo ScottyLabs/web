e4c71cca4ae40ad330c56af3dbca30ee9d3cafc3
Gram Liu <gram.liu226@gmail.com>
2022-08-23 23:04:54 -0400

    Create tech projects page

--- src/icons/Icon/index.tsx
+++ src/icons/Icon/index.tsx
@@ -5,12 +5,15 @@ import FigmaIcon from "../FigmaIcon";
 import FlutterIcon from "../FlutterIcon";
 import IllustratorIcon from "../IllustratorIcon";
 import InstagramIcon from "../InstagramIcon";
+import JavaIcon from "../JavaIcon";
 import JavaScriptIcon from "../JavaScriptIcon";
+import LinkIcon from "../LinkIcon";
 import MailIcon from "../MailIcon";
 import MattermostIcon from "../MattermostIcon";
 import MediumIcon from "../MediumIcon";
 import MongoDbIcon from "../MongoDbIcon";
 import NextJsIcon from "../NextJsIcon";
+import OpenStreetMapIcon from "../OpenStreetMapIcon";
 import ProcreateIcon from "../ProcreateIcon";
 import ReactIcon from "../ReactIcon";
 import ScottyLabsIcon from "../ScottyLabsIcon";
@@ -24,12 +27,15 @@ const IconMapping = {
   flutter: <FlutterIcon />,
   illustrator: <IllustratorIcon />,
   instagram: <InstagramIcon />,
+  java: <JavaIcon />,
   javascript: <JavaScriptIcon />,
+  link: <LinkIcon />,
   mail: <MailIcon />,
   mattermost: <MattermostIcon />,
   mongodb: <MongoDbIcon />,
   medium: <MediumIcon />,
   nextjs: <NextJsIcon />,
+  openstreetmap: <OpenStreetMapIcon />,
   procreate: <ProcreateIcon />,
   react: <ReactIcon />,
   scottylabs: <ScottyLabsIcon />,
