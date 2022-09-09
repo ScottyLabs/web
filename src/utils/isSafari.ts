8beaa7e8fc64bc6f3a855d8313b26ae38d1b5333
Gram Liu <gram.liu226@gmail.com>
2022-09-08 22:58:26 -0400

    Restore default safari condition

--- src/utils/isSafari.ts
+++ src/utils/isSafari.ts
@@ -6,7 +6,7 @@ export default function isSafari() {
     return false;
   }
   if (navigator.userAgent.indexOf("Chrome") > -1) {
-    return true;
+    return false;
   }
   return navigator.userAgent.indexOf("Safari") > -1;
 }
