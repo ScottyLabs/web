98e4b9aa9ff311e0a35f68f19fc0706ae9793499
GhostOf0days <jaisal.patel45@gmail.com>
2024-07-31 18:10:48 -0400

    added sponsorship packet

---
+++ src/pages/sponsors.tsx
@@ -0,0 +1,27 @@
+import BaseLayout from 'layouts/BaseLayout';
+import styles from 'styles/Sponsors.module.scss';
+
+export default function Sponsors() {
+	const sponsorPacketURL = 'Sponsorship_24_25.pdf';
+
+	return (
+		<BaseLayout collapsedNavBar>
+			<div className={styles.pageContent}>
+				<div className={styles.sponsorPacketContainer}>
+					<object
+						data={sponsorPacketURL}
+						type="application/pdf"
+						className={styles.sponsorPacket}
+					>
+						<p>
+							You dont have a PDF viewer for this browser. You can{' '}
+							<a href={sponsorPacketURL}>
+								click here to download the sponsor packet.
+							</a>
+						</p>
+					</object>
+				</div>
+			</div>
+		</BaseLayout>
+	);
+}
