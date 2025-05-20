cca00f5f1511676eebf673d620a90728005498b3
Anish Pallati <anishp@duck.com>
2025-05-20 14:15:54 -0400

    feat: add clerk authorized redirect origins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCircle as faCircleHollow } from '@fortawesome/free-regular-svg-icons';
 import { faBars, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 import 'styles/globals.scss';
 
@@ -8,7 +9,17 @@ function MyApp({ Component, pageProps }: AppProps) {
 	library.add(faBars);
 	library.add(faCircle);
 	library.add(faCircleHollow);
-	return <Component suppressHydrationWarning {...pageProps} />;
+
+	return (
+		<ClerkProvider
+			allowedRedirectOrigins={[
+				'https://www.cmucourses.com',
+				'https://cmucourses.com',
+			]}
+		>
+			<Component suppressHydrationWarning {...pageProps} />
+		</ClerkProvider>
+	);
 }
 
 export default MyApp;
