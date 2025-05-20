4ae3b37f44cd0ae78d79aee58b624ddd69f9c08d
Anish Pallati <anishp@duck.com>
2025-05-20 14:43:50 -0400

    fix: remove next.js rewrites (#86)

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	rewrites: async () => [
-		{
-			source: '/:path*',
-			destination: '/',
-		},
-	],
 };
 
 export default nextConfig;
